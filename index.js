50eeddf2a1b3526c558d6d88fe2e96e709ca985e
Joseph Papaluca <jpapaluc@gmail.com>
2017-09-27 14:06:21 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+  // SCROLLIFY FUNCTIONS:
+  $.scrollify( {
+    section: ".section",
+    scrollbars: false,
+  })
+
+  // $('.one').click(function() {
+  //   $(this).css("background", "pink");
+  // })
+  //
+  // $('.two').click(function() {
+  //   $(this).css("background", "cyan");
+  // })
+
+})
