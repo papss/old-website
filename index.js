a1a6be8864478af64847220e4385f4e7e409d939
Joseph Papaluca <jpapaluc@gmail.com>
2017-09-28 20:30:49 -0400

    redid menu function, html structure

--- index.js
+++ index.js
@@ -4,42 +4,30 @@ $(document).ready(function() {
   // selects sections:
   $.scrollify({
     section: ".section",
-    interstitialSection: ".footer", 
+    interstitialSection: ".footer",
     scrollbars: false,
+    before: function(i, sections) {
+
+      var ref = sections[i].attr("data-section-name");
+      $(".menu .active").removeClass("active");
+      $(".menu").find("a[href=\"#" + ref + "\"]").addClass("active");
+
+    },
+    afterRender: function() {
+
+      var menu = "<ol class=\"menu\">";
+      var activeClass = "";
+      $(".section").each(function(i) {
+        activeClass = "";
+        if(i===0) {
+          activeClass = "active";
+        }
+        menu += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span>" + $(this).attr("data-section-name") + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+      });
+      menu += "</ol>";
+      $(".one").append(menu);
+      $(".menu a").on("click", $.scrollify.move);
+    }
+
   });
-
-  // navigation:
-  $(".first").click(
-    function() {
-      $.scrollify.move("#1");
-    }
-  );
-
-  $(".second").click(
-    function() {
-      $.scrollify.move("#2");
-    }
-  );
-
-  $(".third").click(
-    function() {
-      $.scrollify.move("#3");
-    }
-  );
-
-  $(".fourth").click(
-    function() {
-      $.scrollify.move("#4");
-    }
-  );
-
-  // OTHER FUNCTIONS:
-  $("li").hover(
-    function() {
-      $(this).css("color", "white");
-    }, function() {
-      $(this).css("color", "black");
-    }
-  );
-
-})
+});
