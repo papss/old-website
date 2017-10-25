7fbabe8d153e47b2e230fb660b7f6ca3db19c691
Joseph Papaluca <jpapaluc@gmail.com>
2017-10-24 21:09:57 -0400

    restructuring html, css

--- index.js
+++ index.js
@@ -3,32 +3,7 @@ $(document).ready(function() {
   // SCROLLIFY FUNCTIONS:
   // selects sections:
   $.scrollify({
-    section: ".section",
-    interstitialSection: ".footer",
-    scrollbars: false,
-    before: function(i, sections) {
-
-      var ref = sections[i].attr("data-section-name");
-      $(".menu .active").removeClass("active");
-      $(".menu").find("a[href=\"#" + ref + "\"]").addClass("active");
-
-    },
-    // renders the nav menu:
-    afterRender: function() {
-
-      var menu = "<ol class=\"menu\">";
-      var activeClass = "";
-      $(".section").each(function(i) {
-        activeClass = "";
-        if(i===0) {
-          activeClass = "active";
-        }
-        menu += "<li><a class=\"nav " + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span>" + $(this).attr("data-section-name") + "</span></a></li>";
-      });
-      menu += "</ol>";
-      $(".left").append(menu);
-      $(".menu a").on("click", $.scrollify.move);
-    }
-
+    section: "section",
+    scrollbars: false
   });
 });
